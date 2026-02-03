0c53c2246128b2b2af0fcc314532c8922b21da8a
Paweł Grabacki <paw.grabacki@gmail.com>
2026-02-04 00:06:42 +0100

    update

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Jenkins + Flask 🚀 15930"
+    return "Hello World from Jenkins + Flask 🚀 15939"
 
 
 if __name__ == "__main__":
