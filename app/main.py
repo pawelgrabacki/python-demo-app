9e762addb8774ede4eb419bf019e5fe6fd2e803f
Paweł Grabacki <paw.grabacki@gmail.com>
2026-02-06 20:18:04 +0100

    updated main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,10 +6,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return (
-        "Hello World from GCP Compute instance with Docker Image containing simple Flask app 15939"
-        "Paweł Grabacki"
-    )
+    build_number = os.getenv("BUILD_NUMBER", "unknown")
+
+    return f"""
+    <html>
+        <head><title>python-demo-app</title></head>
+        <body>
+            <h1>Hello from Flask</h1>
+
+            <h3>
+              Hello World from GCP Compute instance with Docker Image containing simple Python/Flask app
+              <br>
+              Paweł Grabacki
+            </h3>
+
+            <p><b>Jenkins Build Number:</b> {build_number}</p>
+
+            <p>This is HTML returned directly from main.py</p>
+
+            <ul>
+              <li><a href="https://github.com/pawelgrabacki/python-demo-app">
+                github/pawelgrabacki/python-demo-app
+              </a></li>
+
+              <li><a href="https://hub.docker.com/r/pawelgrabacki/python-demo-app">
+                dockerhub/pawelgrabacki/python-demo-app
+              </a></li>
+            </ul>
+
+            <footer>demo footer</footer>
+        </body>
+    </html>
+    """
 
 
 if __name__ == "__main__":
