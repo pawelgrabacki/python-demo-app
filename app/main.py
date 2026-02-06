94c026786c22e52040d1499a97e629532f01f63e
Paweł Grabacki <paw.grabacki@gmail.com>
2026-02-06 21:48:23 +0100

    update

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    build_number = os.getenv("BUILD_NUMBER", "unknown")
 
     return f"""
     <html>
@@ -20,7 +19,7 @@ def hello():
               Paweł Grabacki
             </h3>
 
-            <p><b>Jenkins Build Number:</b> {build_number}</p>
+            <p><b>Jenkins Build Number:</b></p>
 
             <p>This is HTML returned directly from main.py</p>
 
